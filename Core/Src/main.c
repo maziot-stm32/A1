3211b0d2d6a2c29bc49e778d8083216d6dcdf397
mz8023yt <mz8023yt@163.com>
2019-12-27 00:22:41 +0800

    [FEAT] 添加LED测试代码

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -20,6 +20,41 @@
 void SystemClock_Config(void);
 static void MX_GPIO_Init(void);
 
+#define LED0_Pin        GPIO_PIN_8
+#define LED0_GPIO_Port  GPIOA
+#define LED1_Pin        GPIO_PIN_2
+#define LED1_GPIO_Port  GPIOD
+
+#define LED_ON          GPIO_PIN_RESET
+#define LED_OFF         GPIO_PIN_SET
+
+/**
+ * @brief  Init led
+ * @retval None
+ */
+void led_init(void)
+{
+    GPIO_InitTypeDef GPIO_InitStruct = {0};
+
+    /* GPIO Ports Clock Enable */
+    __HAL_RCC_GPIOD_CLK_ENABLE();
+    __HAL_RCC_GPIOA_CLK_ENABLE();
+
+    /*Configure GPIO pin : LED0_Pin */
+    GPIO_InitStruct.Pin = LED0_Pin;
+    GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
+    GPIO_InitStruct.Pull = GPIO_PULLUP;
+    GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
+    HAL_GPIO_Init(LED0_GPIO_Port, &GPIO_InitStruct);
+
+    /*Configure GPIO pin : LED1_Pin */
+    GPIO_InitStruct.Pin = LED1_Pin;
+    GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
+    GPIO_InitStruct.Pull = GPIO_PULLUP;
+    GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
+    HAL_GPIO_Init(LED1_GPIO_Port, &GPIO_InitStruct);
+}
+
 /**
  * @brief  The application entry point.
  * @retval int
@@ -29,12 +64,17 @@ int main(void)
     HAL_Init();
     SystemClock_Config();
     MX_GPIO_Init();
+    led_init();
 
     while (1)
     {
-
+        HAL_GPIO_WritePin(LED0_GPIO_Port, LED0_Pin, LED_ON);
+        HAL_GPIO_WritePin(LED1_GPIO_Port, LED1_Pin, LED_ON);
+        HAL_Delay(200);
+        HAL_GPIO_WritePin(LED0_GPIO_Port, LED0_Pin, LED_OFF);
+        HAL_GPIO_WritePin(LED1_GPIO_Port, LED1_Pin, LED_OFF);
+        HAL_Delay(200);
     }
-    /* USER CODE END 3 */
 }
 
 /**
@@ -73,8 +113,8 @@ void SystemClock_Config(void)
 }
 
 /**
- * @brief GPIO Initialization Function
- * @param None
+ * @brief  GPIO Initialization Function
+ * @param  None
  * @retval None
  */
 static void MX_GPIO_Init(void)
