ec648075337a53f04a34664772d0ab3cdc03e48b
mz8023yt <mz8023yt@163.com>
2019-12-27 01:09:40 +0800

    [CONF] 增加产品定义宏, 后续用于兼容A1和A2设备

---
+++ MAZ_Common/MAZ_Com_product.h
@@ -0,0 +1,17 @@
+/*
+ * MAZ_Com_product.h
+ *
+ *  Created on: 2019年12月27日
+ *      Author: paul@maziot.com
+ */
+
+#ifndef MAZ_COM_PRODUCT_H_
+#define MAZ_COM_PRODUCT_H_
+
+/* Products lists */
+// A1: stm32f103rc minimum system board
+// A2: stm32f103c8 minimum system board
+
+#define MAZ_COM_PRODUCT         A1
+
+#endif /* MAZ_COM_PRODUCT_H_ */
